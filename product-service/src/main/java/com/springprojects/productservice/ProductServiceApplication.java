f28400f1a9e896a1057b9c31994d825669122728
Tomasz Wasielewski <79154397+tom-wasi@users.noreply.github.com>
2023-11-23 11:53:09 +0100

    Added discovery server and it's static resources routes to api gateway.

--- product-service/src/main/java/com/springprojects/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/springprojects/productservice/ProductServiceApplication.java
@@ -8,8 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
 }
